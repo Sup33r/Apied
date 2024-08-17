0be4e601dcc1a89fbd008035bd032114c85e86a9
Super <55873818+Sup33r@users.noreply.github.com>
2024-08-17 15:15:25 +0200

    Initial commit

---
+++ src/main/java/live/supeer/apied/Apied.java
@@ -0,0 +1,17 @@
+ package live.supeer.apied;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Apied extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
